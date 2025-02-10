6d3efa8ace241ad012a0ef266f2498bbfc15ae1f
Mark Christiansen <machr@users.noreply.github.com>
2025-02-10 20:41:35 +1100

    Fix Cook link

--- src/app/cooks/page.tsx
+++ src/app/cooks/page.tsx
@@ -1,4 +1,5 @@
 import { cooks } from '@/app/lib/mockData'
+import Link from 'next/link'
 import {
     Card
 } from "@/components/ui/card"
@@ -8,12 +9,14 @@ export default async function CooksPage() {
         <>
             <h1 className='text-3xl font-bold'>Local cooks in Prahran</h1>
             <div className="container  mx-auto grid grid-cols-4 gap-5">
-                {cooks.map((cook) => (
-                    <Card key={cook.cookId} className="rounded shadow">
-                        <div>
-                            <img src={cook.imageUrl} /></div>
-                        <p>{cook.cookName}</p>
-                    </Card>
+                {cooks.map(cook => (
+                    <Link key={cook.cookId} href={`cooks/${cook.cookId}`}>
+                        <Card className="rounded shadow">
+                            <div>
+                                <img src={cook.imageUrl} /></div>
+                            <p>{cook.cookName}</p>
+                        </Card>
+                    </Link>
                 ))}
             </div>
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold">Go to cooks!</h1>
-      <Link href="/cooks">Find some food</Link>
+      <h1 className="text-3xl font-bold mb-6">Go to cooks!</h1>
+      <Link href="/cooks" className="text-3xl font-bold bg-black text-white p-2 rounded">Find some food</Link>
     </>
   );
 }
