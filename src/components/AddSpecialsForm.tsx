f1840f118202b0dcd6b3baadf754f1c873d1bfb5
Mark Christiansen <machr@users.noreply.github.com>
2025-02-11 15:29:21 +1100

    Add more form fields

--- src/components/AddSpecialsForm.tsx
+++ src/components/AddSpecialsForm.tsx
@@ -14,26 +14,41 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+
+
+
+const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', ' Friday', 'Saturday', 'Sunday'] as const;
+const PRICE_RANGE = ['cheap', 'average', 'exxy'] as const;
 const formSchema = z.object({
-    username: z.string().min(2, {
+    venueName: z.string().min(2, {
         message: "Username must be at least 2 characters.",
     }),
+    specialsDay: z.enum(DAYS),
+    priceRange: z.enum(PRICE_RANGE)
 })
 
 function AddSpecialsForm() {
-    // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            username: "",
+            venueName: "",
+            specialsDay: "Monday",
+            priceRange: "average"
         },
     })
     const { watch } = form;
 
-    // 2. Define a submit handler.
     function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values)
     }
@@ -43,25 +58,63 @@ function AddSpecialsForm() {
 
     return (
         <>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <Form {...form} >
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mb-5">
                     <FormField
                         control={form.control}
-                        name="username"
+                        name="venueName"
                         render={({ field }) => (
                             <FormItem>
-                                <FormLabel>Username</FormLabel>
+                                <FormLabel>Venue Name</FormLabel>
                                 <FormControl>
-                                    <Input placeholder="shadcn" {...field} />
+                                    <Input placeholder="Add venue name" {...field} />
                                 </FormControl>
                                 <FormDescription>
-                                    This is your public display name.
+                                    This is the public venue name
                                 </FormDescription>
                                 <FormMessage />
                             </FormItem>
                         )}
                     />
-                    <Button type="submit">Submit</Button>
+                    <FormField
+                        control={form.control}
+                        name="specialsDay"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Venue Name</FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Select which day the special is on" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {DAYS.map((day) => (
+                                            <SelectItem key={day} value={day.toLowerCase()}>{day}</SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <FormDescription>
+                                    Which day is the special on?
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField control={form.control} name="priceRange" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Price Range</FormLabel>
+                            <RadioGroup defaultValue="option-one">
+                                {PRICE_RANGE.map(option => (
+                                    <div key={option} className="flex items-center space-x-2">
+                                        <RadioGroupItem value={option} id={option} />
+                                        <Label className="capitalize" htmlFor={option}>{option}</Label>
+                                    </div>
+                                ))}
+                            </RadioGroup>
+                        </FormItem>
+                    )} />
+                    <Button className="mb-5" type="submit">Submit</Button>
                 </form>
             </Form>
 
